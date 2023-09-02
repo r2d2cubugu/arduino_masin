227375444cb2dcfd2ee21702b3da66c21c7b6754
r2d2cubugu <131458397+r2d2cubugu@users.noreply.github.com>
2023-09-02 15:02:33 +0400

    Arduino_masin_kodu.ino

--- arduino_masin_kodu/arduino_masin_kodu.ino
+++ arduino_masin_kodu/arduino_masin_kodu.ino
@@ -43,14 +43,14 @@ int redColor = 0;
 int greenColor = 0;
 int blueColor = 0;
 int nofilterColor = 0;
-int Round=0;
+int Round = 0;
 // int blueFrequency = 0;
 void setup() {
-// put your setup code here, to run once:
-// qtr.setTypeAnalog();
-// qtr.setSensorPins((const uint8_t[]){ A0 }, 1);
-// qtr.setEmitterPin(1);
-// pinMode(yer, INPUT_PULLUP);
+  // put your setup code here, to run once:
+  // qtr.setTypeAnalog();
+  // qtr.setSensorPins((const uint8_t[]){ A0 }, 1);
+  // qtr.setEmitterPin(1);
+  // pinMode(yer, INPUT_PULLUP);
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
@@ -58,7 +58,7 @@ void setup() {
   pinMode(DS1, INPUT_PULLUP);
   pinMode(DS2, INPUT_PULLUP);
   pinMode(DS3, INPUT_PULLUP);
-  
+
   // Setting the sensorOut as an input
   pinMode(sensorOut, INPUT);
   pinMode(solpwm1, OUTPUT);
@@ -74,32 +74,32 @@ void setup() {
 
   delay(2000);
 }
-int donmeistiqameti=0; //0 olanda sola dönür 1 olanda sağa dönür
+int donmeistiqameti = 0;  //0 olanda sola dönür 1 olanda sağa dönür
 int donmevaxti = 200;
 
 void loop() {
   if (digitalRead(DS1) == 1 && digitalRead(DS2) == 1) {
-donmeistiqameti=0;
-   }else if(digitalRead(DS1) == 0 && digitalRead(DS2) == 0){
-     donmeistiqameti=1;
-   }
+    donmeistiqameti = 0;
+  } else if (digitalRead(DS1) == 0 && digitalRead(DS2) == 0) {
+    donmeistiqameti = 1;
+  }
 
-if(digitalRead(DS3) == 0){
-  Round=1 ;
-}else{
-  Round=2 ;
-}
+  if (digitalRead(DS3) == 0) {
+    Round = 1;
+  } else {
+    Round = 2;
+  }
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
 
   redFrequency = pulseIn(sensorOut, LOW);
-   redColor = map(redFrequency, 253, 750, 255,0);
+  redColor = map(redFrequency, 253, 750, 255, 0);
 
   Serial.print("R = ");
   Serial.print(redColor);
-    Serial.print("  ");
+  Serial.print("  ");
   Serial.print(redFrequency);
-  
+
   delay(100);
 
   digitalWrite(S2, HIGH);
@@ -110,64 +110,84 @@ if(digitalRead(DS3) == 0){
 
   Serial.print(" G = ");
   Serial.print(greenColor);
-    Serial.print("  ");
+  Serial.print("  ");
   Serial.print(greenFrequency);
   delay(100);
 
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
-  
+
   // Reading the output frequency
   blueFrequency = pulseIn(sensorOut, LOW);
   blueColor = map(blueFrequency, 269, 855, 255, 0);
-  // Printing the BLUE (B) value 
+  // Printing the BLUE (B) value
   Serial.print(" B = ");
   Serial.print(blueColor);
   Serial.print("  ");
   Serial.println(blueFrequency);
-  
+
   delay(100);
 
-  digitalWrite(S2, HIGH );
+  digitalWrite(S2, HIGH);
   digitalWrite(S3, LOW);
-  
+
   // Reading the output frequency
 
- 
+
 
   get(100, 100);
   if (digitalRead(onsensor) == LOW) {
-    if(round==1){
-      if(donmeistiqameti==0){
-        Soladon(1200);
-      }else if(donmeistiqameti==1){
-        Sagadon(1200);
-      }
-}else if(round==2){
-   if(redColor>greenColor&&blueColor>redColor){
-    Sagadon(400);
-  }else if(blueColor>redColor&&blueColor>greenColor){
-     if(donmeistiqameti==0){
-        Soladon(1200);
-      }else if(donmeistiqameti==1){
-        Sagadon(1200);
-      }
-  }else{
-    Soladon(400);
-  }
-}
+    if(blueColor > redColor && blueColor > greenColor){
+    // if(round==1){
+    Serial.print(" Gorur ");
+
+    get(-80, 100);
+
+    if (donmeistiqameti == 0) {
+      Sagadon(300);
+      
+    } else if (donmeistiqameti == 1) {
+      Soladon(300);
     }
-  if(digitalRead(solsensor) == LOW&&digitalRead(sagsensor) == LOW){
-    get(-50,1);
-    if(donmeistiqameti==0){
-      Soladon(400);
-    }else{
+get(100,100);
+    if (donmeistiqameti == 1) {
+      Sagadon(900);
+      
+    } else if (donmeistiqameti == 0) {
+      Soladon(900);
+    }
+get(100,100);}
+    // }
+    // else if(round==2){
+    else if (redColor > greenColor && blueColor > redColor) {
       Sagadon(400);
+    } else if (greenColor > redColor && blueColor > greenColor) {
+        Soladon(1200);
+      // }
+      // else{
+      //   Soladon(400);
+      // }
+    }else if(greenColor > 200 && redColor < 250){
+      get(-50,10);
+      Soladon(1000);
+    }else if(redColor > 250){
+      get(-50,10);
+      Sagadon(1000);
     }
-    get(100,100);
-    
   }
- else if (digitalRead(solsensor) == LOW) {
+  //Y ve B
+  if (digitalRead(solsensor) == LOW && digitalRead(sagsensor) == LOW) {
+    get(-50, 1);
+    if (donmeistiqameti == 0) {
+      get(100, 10);
+      Sagadon(400);
+    } else {
+      get(100, 10);
+      Soladon(400);
+    }
+    get(100, 100);
+
+  } else if (digitalRead(solsensor) == LOW) {
     Sagadon(donmevaxti);
     get(50, 1);
 
@@ -175,21 +195,19 @@ if(digitalRead(DS3) == 0){
     Soladon(donmevaxti);
     get(50, 1);
   }
-
-
 }
 
 void Soladon(int time) {
-  don(-30, 150);
+  don(-35, 150);
   don(0, time);
-  don(30, 75);
+  don(35, 75);
   don(0, 1);
 }
 
 void Sagadon(int time) {
-  don(30, 150);
+  don(35, 150);
   don(0, time);
-  don(-30, 75);
+  don(-35, 75);
   don(0, 1);
 }
 void get(float suret, int timex) {
@@ -199,7 +217,7 @@ void get(float suret, int timex) {
     digitalWrite(sagpwm2, LOW);
   } else {
     suret = abs(suret);
-    digitalWrite(sagpwm1, LOW);
+    digitalWrite(sagpwm1, LOW);          
     analogWrite(sagpwm2, suret);
   }
   delay(timex);
